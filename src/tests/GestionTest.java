3c93d559ce27069a2b4a071d243a5b377793c42f
lgilucam <lgil@alu.ucam.edu>
2023-01-14 03:40:07 +0100

    Create GestionTest.java

---
+++ src/tests/GestionTest.java
@@ -0,0 +1,54 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class GestionTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	final void testGestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testGestionProductoArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testCargarProductos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testMostrarProductos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testMostrarNombreProductos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testComprarProducto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	final void testMostrarCaja() {
+		fail("Not yet implemented");
+	}
+
+}
